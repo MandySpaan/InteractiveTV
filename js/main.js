e215bf3b79f0cb784dfde8fc456866fd137a2b21
MandySpaan <mandyspaan94@gmail.com>
2024-06-05 19:02:34 +0200

    added ch1 to js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 let change = false;
 let tvScreen = document.getElementById("tv-screen");
 let onOffButton = document.getElementById("on-off-button");
+let numberButton = document.getElementsByClassName("number-button");
+
+let arrayChanels = Array.from(numberButton);
+
+for (let i = 0; i < arrayChanels.length; i++) {
+  arrayChanels[i].addEventListener("click", () => {
+    if (change) {
+      tvScreen.classList.toggle(`ch${i + 1}`);
+    }
+  });
+}
 
 onOffButton.addEventListener("click", () => {
   tvScreen.classList.toggle("on");
+  change = !change;
 });
