11406ed305d3abba2d45b07ad92ed90fed5fc159
MandySpaan <mandyspaan94@gmail.com>
2024-06-06 14:59:58 +0200

    made chanel name change upon changing chanel

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let onOffButton = document.getElementById("on-off-button");
 let numberButton = document.getElementsByClassName("number-button");
 let infoButton = document.getElementById("info-button");
 let tvInfo = document.getElementById("tv-info");
+let chanelName = document.getElementById("chanel-name");
 
 let arrayChanels = Array.from(numberButton);
 
@@ -13,6 +14,7 @@ for (let i = 0; i < arrayChanels.length; i++) {
   arrayChanels[i].addEventListener("click", () => {
     if (change) {
       tvScreen.classList.replace(tvScreen.classList[0], `ch${i + 1}`);
+      chanelName.innerHTML = `Channel ${i + 1}`;
     }
   });
 }
